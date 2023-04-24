c5dcbfbee82fc391d81a28edec91f150be45b969
Yodgorbek <yodgorbeksheraliyev777@gmail.com>
2023-04-24 11:41:36 +0500

    added style.css and app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const qr = document.querySelector(".qr");
+const img = document.querySelector(".qr img");
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+
+let url = "";
+
+btn.addEventListener("click", () => {
+  if (!input.value) {
+    alert("Please insert the text");
+  } else {
+    url = `https://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=250x250`;
+    getQr();
+  }
+});
+
+async function getQr() {
+  const response = await fetch(url);
+  qr.style.display = "block";
+  img.src = url;
+  console.log(img.getAttribute("src"));
+  input.value = "";
+}
